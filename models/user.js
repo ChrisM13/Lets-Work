9445f3574585c5bf84f5a0712bbce2f9c211e747
Chris Mosier <chrismosier13@gmail.com>
2017-12-11 13:33:53 -0800

    set up user model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,46 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
-const userSchema = Schema({
+const SALT_ROUNDS = 6;
 
-})
+const userSchema = Schema({
+    name: String,
+    email: {
+        type: String,
+        require: true,
+        lowercase: true,
+        unique: true
+    },
+    password: String
+    },{
+    timestamps: true
+});
+
+userSchema.set('toJSON', {
+    transform: function(doc, ret) {
+        delete ret.password;
+        return ret;
+    }
+});
+
+userSchema.pre('save', function(next) {
+    //this will be set to the current doc
+    var user = this;
+    if(!user.isModified('password')) return next();
+    //password has been changed - salt and hash it
+    bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
+        if(err) return next(err);
+        user.password = hash;
+        next();
+    });
+});
+
+userSchema.methods.comparePassword = function(tryPassword, cb) {
+    bcrypt.compare(tryPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 module.exports = mongoose.model("User", userSchema)
